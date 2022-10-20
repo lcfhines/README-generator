39a52b9bd52fb557725f26892cc8a8487d46a0a9
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-10-20 17:41:46 -0400

    added functions to return license link and license section if a license is selected

--- README.md
+++ README.md
@@ -1,35 +1,37 @@
 
-  # fun project
+  # README Generator
 
-  ### 
+  ### ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
 
   ## Table of Contents:
-    1. [Description](#Description)
-    2. [Installation](#Installation)
-    3. [Usage](#Usage)
-    4. [Contributing](#Contributing)
-    5. [Tests](#Tests)
-    6. [License](#License)
-    7. [Questions](#Questions)
+  1. [Description](#Description)
+  2. [Installation](#Installation)
+  3. [Usage](#Usage)
+  4. [Contributing](#Contributing)
+  5. [Tests](#Tests)
+  6. [License](#License)
+  7. [Questions](#Questions)
 
   ## Description
-  a really fun project
+  Generates README
 
   ## Installation
-  npm and inquirer
+  Do it
 
   ## Usage
-  to make a readme
+  Use it
 
   ## Contributing
-  idk
+  Contribute to it
 
   ## Tests
-  test
+  Test it
 
+  
   ## License
-  This application is covered by Apache License 2.0.
-
+  This application is covered by [MIT License](https://choosealicense.com/licenses/mit/).
+  
+  
   ## Questions
-  Github: https://github.com/lcfhines  
+  Github profile: [lcfhines](https://github.com/lcfhines)  
   Please reach out by email (lcf7pk@gmail.com) with additional questions.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,16 +3,16 @@
 function renderLicenseBadge(license) {
   let badge = '';
   if (license === 'MIT License'){
-     badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
+     badge = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
   } else
   if (license === 'Apache License 2.0'){
-    badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]';
+    badge = `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
   } else 
   if (license === 'GNU General Public License v3.0'){
-    badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)]';
+    badge = `![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)`;
   } else
   if (license === 'BSD 3'){
-    badge = '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]';
+    badge = `![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)`;
   } else
   if (license === 'None'){
     badge = ''
@@ -22,27 +22,57 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let link = '';
+  if (license === 'MIT License'){
+    link = `https://choosealicense.com/licenses/mit/`;
+ } else
+ if (license === 'Apache License 2.0'){
+    link = `https://choosealicense.com/licenses/apache-2.0/`;
+ } else 
+ if (license === 'GNU General Public License v3.0'){
+    link = `https://choosealicense.com/licenses/gpl-3.0/`;
+ } else
+ if (license === 'BSD 3'){
+    link = `https://choosealicense.com/licenses/bsd-3-clause/`;
+ } else
+ if (license === 'None'){
+    link = ''
+ }
+ return link
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let section = ''
+  if (license === 'MIT License' || license === 'Apache License 2.0' || license === 'GNU General Public License v3.0' || license === 'BSD 3'){
+    section = `
+  ## License
+  This application is covered by [${license}](${renderLicenseLink(license)}).
+  `;
+  } else
+  if (license === 'None'){
+    section = ``;
+  }
+  return section
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
   # ${data.title}
 
-  ### ${renderLicenseBadge(data)}
+  ### ${renderLicenseBadge(data.license)}
 
   ## Table of Contents:
-    1. [Description](#Description)
-    2. [Installation](#Installation)
-    3. [Usage](#Usage)
-    4. [Contributing](#Contributing)
-    5. [Tests](#Tests)
-    6. [License](#License)
-    7. [Questions](#Questions)
+  1. [Description](#Description)
+  2. [Installation](#Installation)
+  3. [Usage](#Usage)
+  4. [Contributing](#Contributing)
+  5. [Tests](#Tests)
+  6. [License](#License)
+  7. [Questions](#Questions)
 
   ## Description
   ${data.description}
@@ -59,11 +89,10 @@ function generateMarkdown(data) {
   ## Tests
   ${data.test}
 
-  ## License
-  This application is covered by ${data.license}.
-
+  ${renderLicenseSection(data.license)}
+  
   ## Questions
-  Github: https://github.com/${data.github}  
+  Github profile: [${data.github}](https://github.com/${data.github})  
   Please reach out by email (${data.email}) with additional questions.
 `
 }
